76b5b6f728f6e72603bab220fc95b93a10d542f2
Tom Ryder <tom@sanctum.geek.nz>
2018-09-11 22:17:44 +1200

    Add line breaks to chapter 4 example

--- 04-input-output-and-redirection/41.bash
+++ 04-input-output-and-redirection/41.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
+
 # Convert all capital letters in the input to lowercase
 tr A-Z a-z > words.lowercase
+
 # Sort all the lowercase words in order
 sort words.lowercase > words.sorted
+
 # Print counts of how many times each word occurs
 uniq -c words.sorted > words.frequency
+
 # Sort that list by frequency, descending
 sort -k1,1nr words.frequency
